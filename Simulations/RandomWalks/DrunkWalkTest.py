fa603d71d238e5e194d248916963104df112d37c
invalid404 <invader_13@mail.ru>
2015-04-05 13:40:09 +0300

    Added drunk walk simulation function

--- Simulations/RandomWalks/DrunkWalkTest.py
+++ Simulations/RandomWalks/DrunkWalkTest.py
@@ -2,3 +2,9 @@ __author__ = 'aDmin'
 
 import DrunkenWalk
 
+def walk(f, d, numSteps):
+    start = f.getLoc(d)
+
+    for step in numSteps
+        f.moveDrunk(d)
+    return (start.distFrom(f.getLoc(d)))
