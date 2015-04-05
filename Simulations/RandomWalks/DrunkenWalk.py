85386b7ce8d4cf82a5cc9946a6ddef62c4a9d0b6
invalid404 <invader_13@mail.ru>
2015-04-05 13:11:38 +0300

    Added Field class

--- Simulations/RandomWalks/DrunkenWalk.py
+++ Simulations/RandomWalks/DrunkenWalk.py
@@ -29,4 +29,26 @@ class Location(object):
     def __str__(self):
         return '<' + str(self.x) + ', ' + str(self.y) + '>'
 
+class Field(object):
 
+    def __init__(self):
+        self.drunks = {}
+
+    def addDrunk(self, drunk, loc):
+        if drunk in self.drunks:
+            raise ValueError('Duplicate drunk')
+        else:
+            self.drunks[drunk] = loc
+
+    def moveDrunk(self, drunk):
+        if not drunk in self.drunks:
+            raise ValueError('Drunk not in field')
+        xDist, yDist = drunk.takeStep()
+        currentLocation = self.drunks[drunk]
+        #use move method of Location to get new location
+        self.drunks[drunk] = currentLocation.move(xDist, yDist)
+
+    def getLoc(self, drunk):
+        if not drunk in self.drunks:
+            raise ValueError('Drunk not in field')
+        return self.drunks[drunk]
