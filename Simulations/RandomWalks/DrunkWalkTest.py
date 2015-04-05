3deafffd9604a8b96d84570ded0fa9aee2c2ccaa
invalid404 <invader_13@mail.ru>
2015-04-05 16:44:34 +0300

    Added to plotting test support of different classes

--- Simulations/RandomWalks/DrunkWalkTest.py
+++ Simulations/RandomWalks/DrunkWalkTest.py
@@ -11,8 +11,8 @@ def walk(f, d, numSteps):
         f.moveDrunk(d)
     return (start.distFrom(f.getLoc(d)))
 
-def simWalks(numSteps, numTrials):
-    homer = DrunkenWalk.UsualDrunk('Homer')
+def simWalks(numSteps, numTrials, dClass=DrunkenWalk.UsualDrunk):
+    homer = dClass('Homer')
     origin = DrunkenWalk.Location(0, 0)
     distances = []
 
@@ -31,24 +31,25 @@ def drunkTest(numTrials):
         print 'Mean = ', sum(distances)/len(distances)
         print 'Max = ', max(distances), ' Min = ', min(distances)
 
-drunkTest(20)
+#drunkTest(20)
 
 def drunkTestPlot(numTrials=50):
-    stepsTaken = [10, 100, 1000, 10000]
-    meanDistances = []
-    squareRoot = []
+    stepsTaken = [10, 100, 1000, 10000, 100000]
+    for dClass in (DrunkenWalk.UsualDrunk, DrunkenWalk.ColdDrunk, DrunkenWalk.EDrunk):
+        meanDistances = []
+        #squareRoot = []
 
-    for numSteps in stepsTaken:
-        distances = simWalks(numSteps,numTrials)
-        meanDistances.append(sum(distances)/len(distances))
-        squareRoot.append(numSteps ** 0.5)
+        for numSteps in stepsTaken:
+            distances = simWalks(numSteps,numTrials)
+            meanDistances.append(sum(distances)/len(distances))
+            #squareRoot.append(numSteps ** 0.5)
 
-    pylab.plot(stepsTaken, meanDistances, label='Mean distances')
-    pylab.plot(stepsTaken, squareRoot, label='Square root of steps')
+        pylab.plot(stepsTaken, meanDistances, label=dClass.__name__)
+    #pylab.plot(stepsTaken, squareRoot, label='Square root of steps')
     pylab.title('Mean distance from Origin')
     pylab.xlabel('Steps taken')
     pylab.ylabel('Steps from Origin')
-    pylab.legend()
+    pylab.legend(bbox_to_anchor=(0.5, 1.05))
     pylab.show()
 
 drunkTestPlot()
