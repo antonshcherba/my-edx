c05e46b7091cfa6a6636b53a874f3147791dbcba
invalid404 <invader_13@mail.ru>
2015-04-05 13:47:38 +0300

    Added drunk walks simulation

--- Simulations/RandomWalks/DrunkWalkTest.py
+++ Simulations/RandomWalks/DrunkWalkTest.py
@@ -5,6 +5,17 @@ import DrunkenWalk
 def walk(f, d, numSteps):
     start = f.getLoc(d)
 
-    for step in numSteps
+    for step in range(numSteps):
         f.moveDrunk(d)
     return (start.distFrom(f.getLoc(d)))
+
+def simWalks(numSteps, numTrials):
+    homer = DrunkenWalk.UsualDrunk('Homer')
+    origin = DrunkenWalk.Location(0, 0)
+    distances = []
+
+    for trial in range(numTrials):
+        f = DrunkenWalk.Field()
+        f.addDrunk(homer, origin)
+        distances.append(walk(f, homer, numTrials))
+    return distances
