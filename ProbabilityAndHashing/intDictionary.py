f4eef8bdcf6397da7cbb905d6834ad7e8ecb99b6
invalid404 <invader_13@mail.ru>
2015-04-16 20:01:44 +0300

    Created functions which calculates collision probability and simulates insertions to hash table

--- ProbabilityAndHashing/intDictionary.py
+++ ProbabilityAndHashing/intDictionary.py
@@ -26,3 +26,106 @@ class intDict(object):
             if e[0] == dictKey:
                 return e[1]
         return None
+
+    def __str__(self):
+        res = '{'
+        for b in self.buckets:
+            for t in b:
+                res = res + str(t[0]) + ':' + str(t[1]) + ','
+        return res[:-1] + '}'
+
+def collision_prob(numBuckets, numInsertions):
+    '''
+    Given the number of buckets and the number of items to insert,
+    calculates the probability of a collision.
+    '''
+    prob = 1.0
+    for i in range(1, numInsertions):
+        prob = prob * ((numBuckets - i) / float(numBuckets))
+    return 1 - prob
+
+def sim_insertions(numBuckets, numInsertions):
+    '''
+    Run a simulation for numInsertions insertions into the hash table.
+    '''
+    choices = range(numBuckets)
+    used = []
+    for i in range(numInsertions):
+        hashVal = random.choice(choices)
+        if hashVal in used:
+            return False
+        else:
+            used.append(hashVal)
+    return True
+
+def observe_prob(numBuckets, numInsertions, numTrials):
+    '''
+    Given the number of buckets and the number of items to insert,
+    runs a simulation numTrials times to observe the probability of a collision.
+    '''
+    probs = []
+    for t in range(numTrials):
+        probs.append(sim_insertions(numBuckets, numInsertions))
+    return 1 - sum(probs)/float(numTrials)
+
+import random
+
+def numBucketsSearch():
+    numBuckets = 365
+    numInsertionsMax = 365
+    numInsertionsMin = 0
+    repeatCount = 0
+    while (True):
+        avg = (numInsertionsMax - numInsertionsMin) / 2 + numInsertionsMin
+        repeatCount +=1
+        if (collision_prob(numBuckets, avg) < 0.99
+            and collision_prob(numBuckets,avg) > 0.98):
+            print 'count =', repeatCount
+            return avg
+        elif (collision_prob(numBuckets, avg) < 0.99):
+            numInsertionsMin = avg
+        elif (collision_prob(numBuckets, avg) > 0.99):
+            numInsertionsMax = avg
+
+
+
+def main():
+    # D = intDict(29)
+    # for i in range(20):
+    #     key = random.choice(range(10**5))
+    #     D.addEntry(key,i)
+    #
+    # print '\n', 'The buckets are:'
+    # for hashBucket in D.buckets:
+    #     print ' ', hashBucket
+
+
+    # print 'Colision probability of ', str(50),\
+    #     ' insertions to ', str(1000),\
+    #     ' buckets is ',collision_prob(numBuckets=1000, numInsertions=50)
+    # print 'Colision probability of ', str(200),\
+    #     ' insertions to ', str(1000),\
+    #     ' buckets is ',collision_prob(numBuckets=1000, numInsertions=200)
+    #
+    # print 'Simulation of ', str(50),\
+    #     ' insertions to ', str(1000),\
+    #     ' buckets is ',observe_prob(numBuckets=1000, numInsertions=50,
+    #                                 numTrials=1000)
+    # print 'Simulation of ', str(200),\
+    #     ' insertions to ', str(1000),\
+    #     ' buckets is ',observe_prob(numBuckets=1000, numInsertions=200,
+    #                                 numTrials=1000)
+    #
+    # print 'Simulation of ', str(30),\
+    #     ' insertions to ', str(365),\
+    #     ' buckets is ',collision_prob(numBuckets=365, numInsertions=30)
+    # print 'Simulation of ', str(250),\
+    #     ' insertions to ', str(365),\
+    #     ' buckets is ',observe_prob(numBuckets=365, numInsertions=250,
+    #                                 numTrials=1000)
+    
+    print numBucketsSearch()
+
+
+if __name__ == '__main__':
+    main()
