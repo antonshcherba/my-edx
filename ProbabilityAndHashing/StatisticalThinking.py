6b516b9e3690d44910c9b29c76eec4cd52bea47c
invalid404 <invader_13@mail.ru>
2015-04-16 20:05:04 +0300

    Added flipPlot function that plots coin flips results

--- ProbabilityAndHashing/StatisticalThinking.py
+++ ProbabilityAndHashing/StatisticalThinking.py
@@ -1,5 +1,8 @@
 __author__ = 'aDmin'
 
+import random
+import pylab
+
 def stdDevOfLengths(L):
     """
     L: a list of strings
@@ -21,11 +24,42 @@ def stdDevOfLengths(L):
 
     return (s / N) ** 0.5
 
+def flipPlot(minExp, maxExp):
+    ratios = []
+    diffs = []
+    xAxis = []
+    for exp in range(minExp,maxExp+1):
+        xAxis.append(2**exp)
+
+    for numFlips in xAxis:
+        numHeads = 0
+        for n in range(numFlips):
+            if random.random() < 0.5:
+                numHeads += 1
+        numTails = numFlips - numHeads
+        ratios.append(numHeads/float(numTails))
+        diffs.append(abs(numHeads - numTails))
+
+    pylab.title('Difference between Heads and Tails')
+    pylab.xlabel('Number of Flips')
+    pylab.ylabel('Abs(Heads - Tails)')
+    pylab.plot(xAxis,diffs, 'o')
+
+    pylab.figure()
+    pylab.title('Heads/Tails ratio')
+    pylab.xlabel('Number of Flips')
+    pylab.ylabel('Heads/Tails')
+    pylab.plot(xAxis,ratios)
+
 def main():
-    print 'Standard deviation of [a, z, p] is',\
-        str(stdDevOfLengths(['a', 'z', 'p']))
-    print 'Standard deviation of [apples, oranges, kiwis, pineapples] is',\
-        str(stdDevOfLengths(['apples', 'oranges', 'kiwis', 'pineapples']))
+    # print 'Standard deviation of [a, z, p] is',\
+    #     str(stdDevOfLengths(['a', 'z', 'p']))
+    # print 'Standard deviation of [apples, oranges, kiwis, pineapples] is',\
+    #     str(stdDevOfLengths(['apples', 'oranges', 'kiwis', 'pineapples']))
+
+    random.seed(2)
+    flipPlot(4, 20)
+    pylab.show()
 
 if __name__ == '__main__':
     main()
