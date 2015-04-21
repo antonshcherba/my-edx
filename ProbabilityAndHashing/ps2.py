5eab46dc8e69667c7d3d6816ecdf7beb0a2fd020
invalid404 <invader_13@mail.ru>
2015-04-21 12:02:22 +0300

    Created class StandardRobot which changes direction after it hits the wall

--- ProbabilityAndHashing/ps2.py
+++ ProbabilityAndHashing/ps2.py
@@ -239,3 +239,36 @@ class Robot(object):
         """
         raise NotImplementedError # don't change this!
 
+
+# === Problem 2
+class StandardRobot(Robot):
+    """
+    A StandardRobot is a Robot with the standard movement strategy.
+
+    At each time-step, a StandardRobot attempts to move in its current
+    direction; when it would hit a wall, it *instead* chooses a new direction
+    randomly.
+    """
+    def updatePositionAndClean(self):
+        """
+        Simulate the raise passage of a single time-step.
+
+        Move the robot to a new position and mark the tile it is on as having
+        been cleaned.
+        """
+        # raise NotImplementedError
+        while (True):
+            newPosition = self.position.getNewPosition(self.direction,
+                                                       self.speed)
+            if (self.room.isPositionInRoom(newPosition)):
+                self.position = newPosition
+                self.room.cleanTileAtPosition(newPosition)
+                break
+            else:
+                self.direction = random.randint(0,360)
+
+
+# Uncomment this line to see your implementation of StandardRobot in action!
+testRobotMovement(StandardRobot, RectangularRoom)
+
+
