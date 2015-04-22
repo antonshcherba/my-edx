5794ead0eab8072883e6d0d4cde10bd9cb3cca52
invalid404 <invader_13@mail.ru>
2015-04-22 17:04:18 +0300

    Added function makeNormal to plot data with normal distribution

--- MonteCarlo/Distributions.py
+++ MonteCarlo/Distributions.py
@@ -1,1 +1,19 @@
 __author__ = 'aDmin'
+
+import random
+import pylab
+
+def makeNormal(mean, sd, numSamples):
+    samples = []
+    for i in range(numSamples):
+        samples.append(random.gauss(mean,sd))
+
+    pylab.hist(samples,bins=101)
+
+def main():
+    makeNormal(4, 8, 100000)
+    pylab.show()
+    return 0
+
+if __name__ == '__main__':
+    main()
